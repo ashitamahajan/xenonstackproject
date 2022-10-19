6fb939ff212567fcfc911fdb1068135b359d98ff
akash0049 <akashmahajan224@gmail.com>
2022-10-19 22:46:46 +0530

    first commit

---
+++ frontend/src/Pages/contact_us.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function ContactUs() {
+  return (
+    <div className="container text-center">
+      <h2 style={{ margin: 30 }}>Contact Us</h2>
+      <div class="card">
+        <div class="card-body">
+          <h5>Phone Number : +91-9876543210</h5>
+          <h5>mail-to : ashitamahajan@gmail.com</h5>
+          <h5>Address : Chandigarh, Punjab, India</h5>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ContactUs;
